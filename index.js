83205825cf28aede9e50fa1d46d93c9155f5f70d
Jacob Sierra <jacobisaac22@yahoo.com>
2021-07-08 14:18:15 -0500

    updated question index and add functions for additional member questions

--- index.js
+++ index.js
@@ -1,31 +1,126 @@
 // packages needed for application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require('./Manager');
+const Enginerr = require('./Engineer');
+const Intern = require('./Intern');
+const Enginer = require('./Engineer');
 
-const questions =
+const team = [];
+
+// prompt questions for Manager
+function questions() {
   inquirer.prompt([
       {
-          message: 'Please build your team',
-          name: 'message'
-      },
-      {
           type:'input',
-          message: "What is the team member's name?",
-          name: 'managers name'
+          message: "What is the team manager's name?",
+          name: 'name'
       },
       {
           type: 'input',
           message: "What is the managers' id?",
-          name: 'managers id',
+          name: 'id',
       },
       {
           type: 'input',
           message: "What is the team manager's email?",
-          name: 'managers email',
+          name: 'email',
       },
       {
           type: 'input',
           message: "What is the team manager's office number?",
-          name: 'managers email',
+          name: 'office number',
       },
-  ]);
+   ])
+     .then(function (answers){
+         let manager = new Manager(answers.name, answers.id, answers.emai, answers.OfficeNumber);
+         team.push(manager)
+         nextMember()
+    })
+     .catch(function(err){
+         console.log(err);
+    })
+};
+// function to chooce next member
+    function nextMember(){
+        let choice = 
+        inquirer.prompt([
+            {
+                type: 'list',
+                message:'Which type of member would you like to add?',
+                choices: ['Engineer', 'Intern', 'I do not want to add anymore'],
+                name: 'choice'
+            }
+        ]);
+// if statement depending if choice is engineer these questions will be prompted
+        if (choice.choice === 'Engineer'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the Engineers name?',
+                    name: 'name',
+                },
+                {
+                    type: 'input',
+                    message: 'What us the Engineers id',
+                    name: 'id',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Engineers email',
+                    email: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Engineers Github?',
+                    name: 'github',
+                },
+            ])
+              .then(function(answers){
+                  var engineer = new Enginer(answers.name, answers.id, answers.email, answers.github);
+                  team.push(engineer)
+                  nextMember()
+            })
+              .catch(function(err){
+                  console.log(err);
+              });    
+        }
+        else if (choice.choice === 'Intern'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'What is the Interns name?',
+                    name: 'name',
+                },
+                {
+                    type: 'input',
+                    message: 'What us the Interns id?',
+                    name: 'id',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Interns email?',
+                    email: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'What is the Interns School?',
+                    name: 'school',
+                },
+            ])
+             .then(function(answers){
+                 var intern = new Intern(answers.name, answers.id, answers.emai, answers.school);
+                 team.push(intern)
+             })
+             .catch(function(err){
+                 console.log(err)
+             });
+        }
+        else {createFile()}
+} 
+
+questions()
+
+function createFile(){
+    fs.writeFile()
+}
